2ad7d7c31178e4c941afe403cc7401a2544f7f7e
Adryan Willian Machado Murmel <135062378+adryanmurmel@users.noreply.github.com>
2023-06-25 18:39:24 -0300

    Add files via upload

---
+++ MaiorValor01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int maior(int A, int B);
+
+int maior(int A, int B)
+{
+  if (A > B)
+    return A;
+  else
+    return B;
+}
+
+int main()
+{
+  int num1, num2;
+
+  printf("Insira dois valores: \n");
+  scanf("%d %d", &num1, &num2);
+
+  printf("O maior valor eh: %d", maior(num1, num2));
+
+  return 0;
+}
