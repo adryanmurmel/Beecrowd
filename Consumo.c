d543854127559c3539f05b1d7cb3bee0c622658f
Adryan Willian Machado Murmel <135062378+adryanmurmel@users.noreply.github.com>
2023-06-26 18:49:31 -0300

    Add files via upload

---
+++ Consumo.c
@@ -0,0 +1,21 @@
+/*Calcule o consumo médio de um automóvel
+sendo fornecidos a distância total percorrida (em Km)
+e o total de combustível gasto (em litros).*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  int x;
+  float y;
+  float c;
+
+  printf("Insira quantos km percorreu e quantos litros gastou: ");
+  scanf("%d %f", &x, &y);
+
+  c = x / y;
+
+  printf("%.3lf km/l\n", c);
+  return 0;
+}
---
+++ NumerosPares.c
@@ -0,0 +1,17 @@
+/*Faça um programa que mostre os números pares entre 1 e 100.*/
+
+#include <stdio.h>
+
+int main()
+{
+  int cont = 0;
+
+  while (cont < 100)
+  {
+    cont = cont + 2;
+    if (cont % 2 == 0)
+      printf("%d\n", cont);
+  }
+
+  return 0;
+}
