5bd8112cc5468cfea8de9910344f9a4c35317f73
Adryan Willian Machado Murmel <135062378+adryanmurmel@users.noreply.github.com>
2023-07-29 19:23:46 -0300

    AreaCirculo.c

--- AreaCirculo.c
+++ AreaCirculo.c
@@ -1,7 +1,7 @@
 /*Entrada
 A entrada contém um valor de ponto flutuante (dupla precisão),
 no caso, a variável raio.
-
+ 
 Saída
 Apresentar a mensagem "AREA=" seguido pelo valor da variável area, 
 conforme exemplo abaixo, com 4 casas após o ponto decimal.
