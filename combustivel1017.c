0fb8dde03e56ee09dc24e6d61ea3084b491ecfc4
Adryan Willian Machado Murmel <135062378+adryanmurmel@users.noreply.github.com>
2023-09-04 20:30:48 -0300

    Add files via upload

---
+++ combustivel1017.c
@@ -0,0 +1,26 @@
+/*Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem,
+ao utilizar um automóvel que faz 12 KM/L. 
+Para isso, ele gostaria que você o auxiliasse através de um simples programa.
+Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas)
+e a velocidade média durante a mesma (em km/h).
+Assim, pode-se obter distância percorrida e, em seguida,
+calcular quantos litros seriam necessários. 
+Mostre o valor com 3 casas decimais após o ponto.*/
+
+#include<stdio.h>
+
+int main()
+{
+	int tempgasto, velmedia;
+	float distanciap, gasto;
+	
+	printf("Insira o tempo gasto em horas: ");
+	scanf("%d", &tempgasto);
+	printf("Insira a velocidade media: ");
+	scanf("%d", &velmedia);
+	
+	distanciap = tempgasto * velmedia;
+	gasto = distanciap / 12;
+	
+	printf("O gasto foi de %.2f", gasto);
+}
