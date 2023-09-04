9c23bb08b57bc18065557411d79daf561e4e7b91
Adryan Willian Machado Murmel <135062378+adryanmurmel@users.noreply.github.com>
2023-09-04 20:31:17 -0300

    combustivel1017.c

--- combustivel1017.c
+++ combustivel1017.c
@@ -1,11 +1,11 @@
-/*Joaozinho quer calcular e mostrar a quantidade de litros de combustÌvel gastos em uma viagem,
-ao utilizar um automÛvel que faz 12 KM/L. 
-Para isso, ele gostaria que vocÍ o auxiliasse atravÈs de um simples programa.
-Para efetuar o c·lculo, deve-se fornecer o tempo gasto na viagem (em horas)
-e a velocidade mÈdia durante a mesma (em km/h).
-Assim, pode-se obter dist‚ncia percorrida e, em seguida,
-calcular quantos litros seriam necess·rios. 
-Mostre o valor com 3 casas decimais apÛs o ponto.*/
+/*Joaozinho quer calcular e mostrar a quantidade de litros de combust√≠vel gastos em uma viagem,
+ao utilizar um autom√≥vel que faz 12 KM/L. 
+Para isso, ele gostaria que voc√™ o auxiliasse atrav√©s de um simples programa.
+Para efetuar o c√°lculo, deve-se fornecer o tempo gasto na viagem (em horas)
+e a velocidade m√©dia durante a mesma (em km/h).
+Assim, pode-se obter dist√¢ncia percorrida e, em seguida,
+calcular quantos litros seriam necess√°rios. 
+Mostre o valor com 3 casas decimais ap√≥s o ponto.*/
 
 #include<stdio.h>
 
@@ -22,5 +22,5 @@ int main()
 	distanciap = tempgasto * velmedia;
 	gasto = distanciap / 12;
 	
-	printf("O gasto foi de %.2f", gasto);
+	printf("O gasto foi de %.3f", gasto);
 }
