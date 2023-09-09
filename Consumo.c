7a11d3886bdf349b5bec61d82c07daf69be7935b
Adryan Willian Machado Murmel <135062378+adryanmurmel@users.noreply.github.com>
2023-09-09 13:02:28 -0300

    Consumo.c

--- Consumo.c
+++ Consumo.c
@@ -1,7 +1,7 @@
 /*Calcule o consumo médio de um automóvel
 sendo fornecidos a distância total percorrida (em Km)
 e o total de combustível gasto (em litros).*/
-
+ 
 #include <stdio.h> 
 #include <math.h> 
  
